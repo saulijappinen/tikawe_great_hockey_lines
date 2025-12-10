71326322b5728b301c35e17f93fb81b17b168ffe
Sauli Jäppinen <saulijappinen@gmail.com>
2025-12-10 15:20:31 +0200

    visits table removed, comments cleaned

--- sql/schemas.sql
+++ sql/schemas.sql
@@ -1,12 +1,6 @@
--- visit to site (index)
-CREATE TABLE visits (
-    id INTEGER PRIMARY KEY, -- id_visit!!
-    visited_at TEXT
-);
-
 -- users and hashed pw
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY, -- change to id_user!
+    id INTEGER PRIMARY KEY, 
     username TEXT UNIQUE,
     password_hash TEXT, 
     creation_time TEXT 
@@ -14,8 +8,8 @@ CREATE TABLE users (
 
 
 CREATE TABLE items (
-    id INTEGER PRIMARY KEY, -- change to id_item!!
-    linename TEXT UNIQUE, -- but only unique name
+    id INTEGER PRIMARY KEY, 
+    linename TEXT UNIQUE, 
     player_lw TEXT, -- same player can be in multiple lines!
     player_c TEXT,
     player_rw TEXT,
